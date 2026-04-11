6a04915cc7146194ffde5d978a8297215bd62cbf
Alena <mikaze2002@mail.ru>
2026-04-11 04:06:24 +0300

    Добавила if/else

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
 const (
 	usdToEur = 0.94
@@ -8,25 +12,59 @@ const (
 	eurToRub = usdToRub / usdToEur
 )
 
-func getUserInput(promt string) string {
-	var input string
-	fmt.Print(promt)
-	fmt.Scan(&input)
-	return input
-}
+func inputCurrency(prompt string) string {
+	for {
+		fmt.Print(prompt + " (USD, EUR, RUB): ")
+		var input string
+		fmt.Scan(&input)
+		input = strings.ToUpper(input)
 
-func calculateConversion(amount string, fromCurrency string, toCurrency string) {
-	fmt.Printf("Расчет: %s из %s в %s (функция пока пуста)\n", amount, fromCurrency, toCurrency)
+		if input == "USD" || input == "EUR" || input == "RUB" {
+			return input
+		}
+		fmt.Println("Ошибка! Введите правильно: USD, EUR или RUB")
+	}
+}
+func inputAmount() float64 {
+	for {
+		fmt.Print("Введите сумму: ")
+		var input string
+		fmt.Scan(&input)
+
+		amount, err := strconv.ParseFloat(input, 64)
+		if err == nil && amount >= 0 {
+			return amount
+		}
+		fmt.Println("Ошибка! Введите число (например, 100.50)")
+	}
 }
 func main() {
+	fmt.Println("--- Меню конвертации ---")
 
-	fmt.Println("Курсы валют:")
-	fmt.Println("USD -> EUR =", usdToEur)
-	fmt.Println("USD -> RUB =", usdToRub)
-	fmt.Println("EUR -> Rub =", eurToRub)
-	fmt.Println("-------------------------")
-	amount := getUserInput("Введите сумму для конвертации: ")
-	fromVal := getUserInput("Введите исходную валюту (например, USD): ")
-	toVal := getUserInput("Введите целевую валюту (например, EUR): ")
-	calculateConversion(amount, fromVal, toVal)
+	amount := inputAmount()
+
+	from := inputCurrency("Выберите исходную валюту")
+
+	to := inputCurrency("Выберите целевую валюту")
+
+	var result float64
+
+	var amountInUSD float64
+	if from == "USD" {
+		amountInUSD = amount
+	} else if from == "EUR" {
+		amountInUSD = amount / usdToEur
+	} else {
+		amountInUSD = amount / usdToRub
+	}
+
+	if to == "USD" {
+		result = amountInUSD
+	} else if to == "EUR" {
+		result = amountInUSD * usdToEur
+	} else {
+		result = amountInUSD * usdToRub
+	}
+
+	fmt.Printf("\nИтог: %.2f %s = %.2f %s\n", amount, from, result, to)
 }
